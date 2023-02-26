cbfd52a5552ceb1b13ade31a979823485ecbfe6d
waohqc <1354214576@qq.com>
2023-02-27 02:07:51 +0800

    init

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Translater' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Translater
+
+end
+
+target 'TranslaterExtension' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  #use_frameworks!
+
+  # Pods for TranslaterExtension
+  pod 'AFNetworking', '~> 4.0'
+end
---
+++ TranslaterExtension/SourceEditorExtension.m
@@ -0,0 +1,27 @@
+//
+//  SourceEditorExtension.m
+//  TranslaterExtension
+//
+//  Created by qiancheng on 2023/2/26.
+//
+
+#import "SourceEditorExtension.h"
+#import "TranslaterDefine.h"
+@implementation SourceEditorExtension
+
+/*
+*/
+- (void)extensionDidFinishLaunching
+{
+    XTLog(@"%s",__FUNCTION__);
+}
+
+/*
+ - (NSArray <NSDictionary <XCSourceEditorCommandDefinitionKey, id> *> *)commandDefinitions
+ {
+ // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+ return @[];
+ }
+*/
+
+@end
