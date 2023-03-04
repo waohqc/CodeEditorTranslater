7fbcf9df44600e05b500e73246c179823ea728aa
waohqc <1354214576@qq.com>
2023-03-05 00:36:35 +0800

    App提示页面

---
+++ Translater/NSFlippedView.h
@@ -0,0 +1,16 @@
+//
+//  NSFlippedView.h
+//  Translater
+//
+//  Created by qiancheng on 2023/3/5.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSFlippedView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
--- TranslaterExtension/SourceEditorCommand.m
+++ TranslaterExtension/SourceEditorCommand.m
@@ -62,7 +62,8 @@
                     selections:(NSArray<XCSourceTextRange *> *)selections
                    transResult:(YouDaoResponse *)transResponse {
     NSInteger line = selections.firstObject.start.line;
-    NSMutableString *appendStr = [[NSMutableString alloc] initWithString:@"#XcodeTranslater:\n"];
+    NSMutableString *appendStr = [[NSMutableString alloc] initWithString:@"#XcodeTranslater:"];
+    if(!transResponse.sentenceTransResult) [appendStr appendString:@"\n"];
     if (transResponse.sentenceTransResult) {
         [appendStr appendString:transResponse.sentenceTransResult.tran];
     } else if (transResponse.wordTransResult){
